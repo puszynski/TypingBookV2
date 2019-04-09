1edef62e8ce87e7138aa553f42ac89e575b02903
puszynski <puszynski@gmail.com>
2019-04-09 17:57:49 +0200

    test PR

--- TypingBook/Controllers/BaseController.cs
+++ TypingBook/Controllers/BaseController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace TypingBook.Controllers
 {
     public class BaseController : Controller
     {
+        // TODO
     }
 }
