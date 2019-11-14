57e3231b5847a2caa9b654f1a12ed520d21c1588
puszynski <puszynski@gmail.com>
2019-11-14 23:54:48 +0100

    Add basic book reedit - bootsrap not ready(!)

--- TypingBook/Services/BookContentService.cs
+++ TypingBook/Services/BookContentService.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
+using TypingBook.Extensions;
 using TypingBook.Repositories.IReporitories;
 using TypingBook.Services.IServices;
 
@@ -20,8 +20,23 @@ namespace TypingBook.Services
 
             foreach (var item in books.Result)
             {
-                //item.Content;
+                item.Content = TransformeBookContent(item.Content);
             }
+        }
+
+        string TransformeBookContent(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return input;
+
+            // replace special char
+            var charsToReplece = new char[] { ' ', ';', ',', '\r', '\t', '\n' };
+            var result = input.Replace(charsToReplece, ' ');
+
+            // replace any kind of whitespace (e.g. tabs, newlines, {doublespaces??} etc.)
+            result = Regex.Replace(result, @"\s+", " ");
+
+            return result;
         }
     }
 }
