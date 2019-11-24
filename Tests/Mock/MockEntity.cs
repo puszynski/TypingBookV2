b26fd0e0319a2778f80b4716cb96e3a769e78fbf
Jakub Puszyński <jakub.puszynski@efitness.com>
2019-11-24 20:23:47 +0100

    Attempt to create repo tests with Moq

---
+++ Tests/Mock/MockEntity.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+using TypingBook.Models;
+
+namespace Tests.Mock
+{
+    internal static class MockEntity
+    {
+        public static Book Book()
+        {
+            var mock = new Mock<Book>();
+            mock.SetupProperty(p => p.Id, 1);
+            mock.SetupProperty(p => p.Title, "Tytuł");
+            mock.SetupProperty(p => p.Content, "Testowa treść książki.");
+            mock.SetupProperty(p => p.Authors, "Jakub Puszyński");
+
+            return mock.Object;
+        }
+    }
+}
---
+++ Tests/Repositories/BookRepositoryTest.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TypingBook.Data;
+using TypingBook.Models;
+using TypingBook.Repositories;
+
+namespace Tests.Repositories
+{
+    class BookRepositoryTest
+    {
+        public Book GetBookByID(int id)
+        {
+            //what if you delete book => exeption in single ??
+            var applicationDbContextMock = new Mock<ApplicationDbContext>();
+            var bookRepository = new BookRepository(applicationDbContextMock.Object);
+            //TODO
+
+
+            return _db.Books.Single(x => x.Id == id);
+        }
+
+
+    }
+}
