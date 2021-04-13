60e813350f599231aa87c13680bd7c0c8ea3918f
jakub.puszynski <jakub.puszynski@efitness.com>
2021-04-13 20:40:25 +0200

    Add blazor server side app

---
+++ TypingBookBlazorApp/ViewModels/TypingViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TypingBookBlazorApp.ViewModels
+{
+    public class TypingViewModel
+    {
+        public int BookId { get; set; }
+        public List<string> BookPages { get; set; }
+        public string BookTitle { get; set; }
+        public string BookAuthors { get; set; }
+        public int CurrentBookPage { get; set; }
+    }
+}
---
+++ TypingBookLibrary/Typing/BookPageSerializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace TypingBookLibrary.Typing
+{
+    public static class BookPageSerializer
+    {
+        public static List<string> GetBookPages(string bookPagesString)
+        {
+            return JsonSerializer.Deserialize<List<string>>(bookPagesString);
+        }
+
+        public static string GetBookPagesString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
